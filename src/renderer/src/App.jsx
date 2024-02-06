f207dbd8f1ae790715b58239d35c353bd14b9a21
Hrithik_wac_react <hrithik.d@webandcrafts.com>
2024-02-06 12:07:32 +0530

    first commit

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,13 @@
+import Login from './components/Login/Login'
+
+function App() {
+  // const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
